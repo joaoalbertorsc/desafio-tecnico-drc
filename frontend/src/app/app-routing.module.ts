c0bb46aaa69e1d77a39825f1f65bd6ea29d400b1
joaoalbertorsc <jaoalbertorsc@gmail.com>
2026-01-29 11:05:33 -0300

    add: create AppRoutingModule to configure and manage application routing

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then(m => m.ProductsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
