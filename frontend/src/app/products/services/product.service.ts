ede86d5b3b35b0dae50bc5e02fa33c5d25f80890
joaoalbertorsc <jaoalbertorsc@gmail.com>
2026-01-29 11:03:58 -0300

    move product-related services and models to 'products' folder for better organization

--- frontend/src/app/products/services/product.service.ts
+++ frontend/src/app/products/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product, ProductResponse } from '../models/product.model';
-import { environment } from '../../environments/environment';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
